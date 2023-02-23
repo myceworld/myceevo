2a5e52273215d01b3aa99ac4456e9873ab875ab7
barrystyle <barrystyle@westnet.com.au>
2023-02-24 01:37:45 +0800

    crypto: add scrypt algorithm and hashing primitives

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -10,7 +10,14 @@
 
 uint256 CBlockHeader::GetHash() const
 {
-    return SerializeHash(*this);
+    if (nVersion > 1)
+        return SerializeHash(*this);
+    return GetPoWHash();
+}
+
+uint256 CBlockHeader::GetPoWHash() const
+{
+    return scrypt_1024_1_1_256(*this);
 }
 
 std::string CBlock::ToString() const
